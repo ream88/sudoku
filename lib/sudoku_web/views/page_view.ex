acad106ae3e0f273b9cae34ac05e2cbe26c12945
Mario Uher <uher.mario@gmail.com>
2021-01-08 20:31:01 +0100

    Add rendering

--- lib/sudoku_web/views/page_view.ex
+++ lib/sudoku_web/views/page_view.ex
@@ -1,3 +1,19 @@
 defmodule SudokuWeb.PageView do
   use SudokuWeb, :view
+
+  def border_classes(x, y) do
+    border_t(x, y) <> " " <> border_r(x, y) <> " " <> border_b(x, y) <> " " <> border_l(x, y)
+  end
+
+  def border_t(_x, y) when y in [1, 4, 7], do: "border-t-4"
+  def border_t(_x, _y), do: "border-t"
+
+  def border_r(x, _y) when x == 9, do: "border-r-4"
+  def border_r(_x, _y), do: ""
+
+  def border_b(_x, y) when y == 9, do: "border-b-4"
+  def border_b(_x, _y), do: ""
+
+  def border_l(x, _y) when x in [1, 4, 7], do: "border-l-4"
+  def border_l(_x, _y), do: "border-l"
 end

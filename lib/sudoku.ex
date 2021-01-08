1cc7e8435ad594827a8a32d11e9034f33940d416
Mario Uher <uher.mario@gmail.com>
2021-01-08 17:49:05 +0100

    Implement basic Sudoku logic

--- lib/sudoku.ex
+++ lib/sudoku.ex
@@ -1,18 +1,85 @@
 defmodule Sudoku do
-  @moduledoc """
-  Documentation for `Sudoku`.
-  """
+  @game [
+    [0, 8, 9, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 3, 0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 0, 1, 8, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 6, 0],
+    [0, 0, 0, 0, 0, 9, 0, 5, 0],
+    [9, 0, 1, 7, 4, 0, 0, 0, 0],
+    [0, 0, 5, 8, 0, 0, 0, 4, 0],
+    [3, 0, 8, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0, 6]
+  ]
 
-  @doc """
-  Hello world.
+  @candidates [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-  ## Examples
+  def game do
+    @game
+  end
 
-      iex> Sudoku.hello()
-      :world
+  def solve(game) do
+    for x <- 1..9, y <- 1..9 do
+      horizontal_candidates = horizontal_candidates(game, x, y)
+      vertical_candidates = vertical_candidates(game, x, y)
+      box_candidates = box_candidates(game, x, y)
 
-  """
-  def hello do
-    :world
+      @candidates
+      |> Enum.filter(fn candidate ->
+        candidate in horizontal_candidates and
+          candidate in vertical_candidates and
+          candidate in box_candidates
+      end)
+      |> IO.inspect(label: "candidates at #{x}:#{y}", charlists: true)
+      |> case do
+        [candidate] -> IO.inspect(candidate, label: "candidate at #{x}:#{y}")
+        _ -> nil
+      end
+    end
+  end
+
+  def horizontal_candidates(game, x, y) do
+    case get(game, x, y) do
+      0 ->
+        used = for y <- 1..9, do: get(game, x, y)
+        @candidates -- used
+
+      _ ->
+        []
+    end
+  end
+
+  def vertical_candidates(game, x, y) do
+    case get(game, x, y) do
+      0 ->
+        used = for x <- 1..9, do: get(game, x, y)
+        @candidates -- used
+
+      _ ->
+        []
+    end
+  end
+
+  def box_candidates(game, x, y) do
+    case get(game, x, y) do
+      0 ->
+        {x, y} = find_box(x, y)
+        used = for x <- x..(x + 2), y <- y..(y + 2), do: get(game, x, y)
+        @candidates -- used
+
+      _ ->
+        []
+    end
+  end
+
+  def find_box(x, y) do
+    {div(x - 1, 3) * 3 + 1, div(y - 1, 3) * 3 + 1}
+  end
+
+  def get(game, x, y) do
+    game |> Enum.at(x - 1) |> Enum.at(y - 1)
+  end
+
+  def solved?(game) do
+    game |> List.flatten() |> Enum.all?(&(&1 > 0))
   end
 end
--- test/sudoku_test.exs
+++ test/sudoku_test.exs
@@ -1,8 +1,39 @@
 defmodule SudokuTest do
   use ExUnit.Case
-  doctest Sudoku
 
-  test "greets the world" do
-    assert Sudoku.hello() == :world
+  @game [
+    [0, 8, 9, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 3, 0, 0, 0, 0, 0],
+    [0, 7, 0, 0, 0, 1, 8, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 6, 0],
+    [0, 0, 0, 0, 0, 9, 0, 5, 0],
+    [9, 0, 1, 7, 4, 0, 0, 0, 0],
+    [0, 0, 5, 8, 0, 0, 0, 4, 0],
+    [3, 0, 8, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0, 6]
+  ]
+
+  test "horizontal_candidates/3" do
+    assert [1, 2, 3, 4, 5, 6, 7] == Sudoku.horizontal_candidates(@game, 1, 1)
+    assert [] == Sudoku.horizontal_candidates(@game, 2, 1)
+    assert [2, 3, 4, 5, 6, 9] == Sudoku.horizontal_candidates(@game, 3, 1)
+  end
+
+  test "vertical_candidates/3" do
+    assert [2, 4, 5, 6, 7, 8] == Sudoku.vertical_candidates(@game, 1, 1)
+    assert [] == Sudoku.vertical_candidates(@game, 1, 2)
+    assert [1, 2, 4, 5, 6, 9] == Sudoku.vertical_candidates(@game, 1, 4)
+  end
+
+  test "box_candidates/3" do
+    assert [2, 3, 4, 5, 6] == Sudoku.box_candidates(@game, 1, 1)
+    assert [] == Sudoku.box_candidates(@game, 2, 1)
+    assert [2, 3, 5, 7, 8, 9] == Sudoku.box_candidates(@game, 9, 8)
+  end
+
+  test "find_box/2" do
+    assert {1, 1} == Sudoku.find_box(1, 1)
+    assert {1, 4} == Sudoku.find_box(3, 5)
+    assert {7, 7} == Sudoku.find_box(9, 9)
   end
 end
